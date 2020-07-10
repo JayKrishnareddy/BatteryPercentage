708e208407bfb6a8211eb86478720dababd4758f
Jay krishna reddy <jayakrishna.majji@us.metrixlab.com>
2020-07-10 11:09:18 +0530

    Add project files.

---
+++ BatteryPercentage/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Management;
+
+namespace BatteryPercentage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Info about Laptop Battery";
+            ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery");
+            foreach(ManagementObject mo in mos.Get())
+            {
+                Console.WriteLine("Battery Name\t:{0}",mo["Name"]);
+                Console.WriteLine("Charge \t\t:{0}%",mo["EstimatedChargeRemaining"]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
