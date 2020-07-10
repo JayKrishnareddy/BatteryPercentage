8a89332e57312315dbfa3adf490e436289065df9
Jay krishna reddy <jayakrishna.majji@us.metrixlab.com>
2020-07-10 11:53:05 +0530

    Added Comments !

--- BatteryPercentage/Program.cs
+++ BatteryPercentage/Program.cs
@@ -7,11 +7,15 @@ namespace BatteryPercentage
     {
         static void Main(string[] args)
         {
+            // Title Display
             Console.Title = "Info about Laptop Battery";
+            //Retrieves a collection of system management objects based on below query !
             ManagementObjectSearcher mos = new ManagementObjectSearcher("select * from Win32_Battery");
             foreach(ManagementObject mo in mos.Get())
             {
+                // To print the Battery name
                 Console.WriteLine("Battery Name\t:{0}",mo["Name"]);
+                // To print the Battery charging status
                 Console.WriteLine("Charge \t\t:{0}%",mo["EstimatedChargeRemaining"]);
             }
             Console.ReadLine();
